c6007fd5ef7a642c85795f737d99e8245814e825
Tim Van Wassenhove <github@timvw.be>
2024-08-19 11:56:10 +0200

    feat: add tracing

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ mod sessions;
 
 use anyhow::Result;
 use axum::Router;
+use tower_http::trace::TraceLayer;
+use tracing::level_filters::LevelFilter;
+use tracing::Level;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+    let subscriber = Registry::default()
+        .with(LevelFilter::from_level(Level::DEBUG))
+        .with(tracing_subscriber::fmt::Layer::default().with_writer(std::io::stdout));
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
 
     // build our application with a route
-    let app = build_router();
+    let app = build_router().layer(TraceLayer::new_for_http());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
