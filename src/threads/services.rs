41abcbe513be2c61d58c1e71b2493091a6b89b9f
Tim Van Wassenhove <github@timvw.be>
2024-08-20 09:37:27 +0200

    feat: replace mutex with arcswap

--- src/threads/services.rs
+++ src/threads/services.rs
@@ -1,7 +1,8 @@
+use std::sync::Arc;
 use crate::threads::model::Thread;
 use anyhow::Result;
+use arc_swap::ArcSwap;
 use axum::async_trait;
-use tokio::sync::Mutex;
 use uuid::Uuid;
 
 #[async_trait]
@@ -11,12 +12,14 @@ pub trait ThreadsService {
 }
 
 pub struct InMemoryThreadsService {
-    threads: Mutex<Vec<Thread>>,
+    //threads: Mutex<Vec<Thread>>,
+    threads: ArcSwap<Vec<Thread>>,
 }
 
 impl InMemoryThreadsService {
     pub fn new() -> Self {
-        let threads = Mutex::new(Vec::new());
+        //let threads = Mutex::new(Vec::new());
+        let threads = ArcSwap::from_pointee(Vec::new());
         InMemoryThreadsService { threads }
     }
 }
@@ -25,13 +28,14 @@ impl InMemoryThreadsService {
 impl ThreadsService for InMemoryThreadsService {
     async fn create(&self) -> Result<Thread> {
         let thread = Thread { id: Uuid::new_v4() };
-        let mut threads = self.threads.lock().await;
+        let mut threads = self.threads.load().to_vec();
         threads.push(thread.clone());
+        self.threads.store(Arc::new(threads));
         Ok(thread)
     }
 
     async fn list(&self) -> Vec<Thread> {
-        let threads = self.threads.lock().await;
-        threads.clone()
+        //let threads = self.threads.lock().await;
+        self.threads.load().to_vec()
     }
 }
